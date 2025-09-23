1c44058a73dce436fc69f0083a2a8420872c7f27
kikkipedia <49567322+kikkipedia@users.noreply.github.com>
2025-09-23 10:55:37 +0200

    First commit

---
+++ src/components/Map.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+onMounted(() => {
+    const map = L.map('map').setView([51.505, -0.09], 3)
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+    }).addTo(map)
+
+    //L.marker([51.505, -0.09]).addTo(map)
+})
+</script>
+
+<style scoped>
+#map {
+    height: 100vh;
+    width: 100vw;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Map from '@/components/Map.vue'
+</script>
+
+<template>
+  <div class="home">
+    <h1>Volcano Emission Atlas</h1>
+    <p>This is a prototype application to visualize volcanic emissions data.</p>
+  <div class="mapView">
+    <Map />
+  </div>
+  </div>
+</template>
+
+<style scoped>
+
+.home {
+    text-align: center;
+    width: 100%;
+    margin-bottom: 2rem;
+}
+
+.mapView {
+    width: 80%;
+    margin: 0 auto;
+    border: 2px solid #ccc;
+    border-radius: 8px;
+    overflow: hidden;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+</style>
+
